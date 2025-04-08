5430dea2248324e7494b5591fdb1bcb7d967840a
Idotzror4 <idotzror4@gmail.com>
2025-04-08 12:02:00 +0300

    My name file modified

--- IdoTzror.java
+++ IdoTzror.java
@@ -1,6 +1,6 @@
 public class IdoTzror{
     public static void main(String[] args){
         for (int i=1;i<=10;i++){
-            System.out.println("Ido Tzror ..." + i);        }
+            System.out.println("Ido Tzror after change ..." + i);        }
     }
 }
